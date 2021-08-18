89e06995dce8e122032341ae5c95cbf8fed6d198
annasabbagh <annamay2491@yahoo.com>
2021-08-18 10:51:11 +0200

    Fixed interval, stopped negative timer

--- src/index.js
+++ src/index.js
@@ -13,17 +13,11 @@ class CountdownTimer {
   }
 
   get hours() {
-    return this.pad(
-      Math.floor(
-        (this.timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-      )
-    );
+    return this.pad(Math.floor((this.timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
   }
 
   get mins() {
-    return this.pad(
-      Math.floor((this.timeDifference % (1000 * 60 * 60)) / (1000 * 60))
-    );
+    return this.pad(Math.floor((this.timeDifference % (1000 * 60 * 60)) / (1000 * 60)));
   }
 
   get secs() {
@@ -31,27 +25,21 @@ class CountdownTimer {
   }
 
   pad(value) {
-    return String(value).padStart(2, "0");
+    return String(value).padStart(2, '0');
   }
 
   count() {
     setInterval(() => {
-      document.querySelector(
-        `${this.selector} [data-value="days"]`
-      ).textContent = this.days;
-      document.querySelector(
-        `${this.selector} [data-value="hours"]`
-      ).textContent = this.hours;
-      document.querySelector(
-        `${this.selector} [data-value="mins"]`
-      ).textContent = this.mins;
-      document.querySelector(
-        `${this.selector} [data-value="secs"]`
-      ).textContent = this.secs;
+      if (this.secs > 0) {
+        document.querySelector(`${this.selector} [data-value="days"]`).textContent = this.days;
+        document.querySelector(`${this.selector} [data-value="hours"]`).textContent = this.hours;
+        document.querySelector(`${this.selector} [data-value="mins"]`).textContent = this.mins;
+        document.querySelector(`${this.selector} [data-value="secs"]`).textContent = this.secs;
+      }
     }, 1000);
   }
 }
 
-const blackFridayTimer = new CountdownTimer("#timer-1", "Nov 26, 2021");
+const blackFridayTimer = new CountdownTimer('#timer-1', 'Nov 26, 2021');
 
 blackFridayTimer.count();
